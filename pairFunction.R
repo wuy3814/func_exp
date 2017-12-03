d58437a2d217975b9cfc2e1c4c7f29a9c187a9d9
Wu Yang <wuy3814@gmail.com>
2017-12-03 22:26:46 +0800

    added three functions

--- pairFunction.R
+++ pairFunction.R
@@ -24,3 +24,31 @@ rankToPairOut <- function(x){
   return(x)
 }
 
+
+# RankShift
+# These two functions transform raw ranking data into a shifted format, in which each column corresponds to one item, and the element represents
+# the rank given to the item
+rankshift <- function(x){
+  xlength <- length(x)
+  xname <- 1:xlength
+  names(xname) <- x
+  ranking <- as.character(1:xlength)
+  out <- as.numeric(xname[ranking])
+  return(out)
+}
+
+rankshiftOut <- function(x){
+  x <- apply(x, 1, rankshift)
+  x <- t(x)
+  x <- as.data.frame(x)
+  return(x)
+}
+###########
+
+# Simplification
+# A simplification function to choose a subset of ranking data, starting from 1 to "lower" (with a default of ending with 1).
+# For example, if I want to subset the ranking of 1:4 items from a 1:12 ranking data, I should run simplification(data, 4)
+simplification <- function(x, lower = NULL){
+  x <- x[x < lower + 1]
+  return(x)
+}
